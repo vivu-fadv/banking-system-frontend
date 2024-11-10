82a270453093bcb5140df9611429a03e20cb6e19
Vinh Vu <121175831+vivu-fadv@users.noreply.github.com>
2024-11-10 10:42:42 -0600

    Update account list component

--- src/app/account-list/account-list.component.ts
+++ src/app/account-list/account-list.component.ts
@@ -1,31 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Account } from '../account';
 
 @Component({
   selector: 'app-account-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './account-list.component.html',
   styleUrl: './account-list.component.css'
 })
-export class AccountListComponent implements OnInit{
+export class AccountListComponent implements OnInit {
     accounts!: Account[];
 
     constructor() {}
 
-    ngOnInit():void{
-      
-        this.accounts = [{
-          "id": 1,
-          "firstName" : "Vinh",
-          "lastName" : "Vu",
-          "email" : "vinh.vu@example.com"
-        },
-        {
-          "id": 2,
-          "firstName" : "John",
-          "lastName" : "Doe",
-          "email" : "john.doe@example.com"
-        }];
+    ngOnInit(): void {
+        this.accounts = [
+          {
+            "id": 1,
+            "firstName": "Vinh",
+            "lastName": "Vu",
+            "email": "vinh.vu@example.com"
+          },
+          {
+            "id": 2,
+            "firstName": "John",
+            "lastName": "Doe",
+            "email": "john.doe@example.com"
+          }
+        ];
     }
 }
